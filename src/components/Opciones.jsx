39e82aaafe8747ba29069382c570110337b8321a
Crypto1776 <plcrypto1776@gmail.com>
2021-08-19 07:28:31 -0700

    icons

--- src/components/Opciones.jsx
+++ src/components/Opciones.jsx
@@ -6,19 +6,28 @@ const opcioneArray = [
     {
         id: '1',
         title: 'Contactos',
-        text: 'Dónde nos puedes encontrar?'
+        text: 'Dónde nos puedes encontrar?',
+        icons: '<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">\n' +
+          '  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />\n' +
+          '</svg>'
     },
     {
         id: '2',
         title: 'Conferencias',
-        text: 'Recibe toda la información de nuestras conferencias.'
+        text: 'Recibe toda la información de nuestras conferencias.',
+        icons: '<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">\n' +
+          '  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />\n' +
+          '</svg>'
 
 
     },
     {
         id: '3',
         title: 'Tienda',
-        text: 'Mire todos nuestros productos.'
+        text: 'Mire todos nuestros productos.',
+        icons: '<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">\n' +
+          '  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />\n' +
+          '</svg>'
 
 
     }
@@ -26,9 +35,10 @@ const opcioneArray = [
 
 const opcionesDisplay = opcioneArray.map((items) => {
     return (
-        <div className="block shadow-xl">
-            <h2 className="text-2xl py-4 font-black">{items.title}</h2>
+        <div className="block shadow-xl bg-green-200 mb-14 md:mb-0">
+            <h2 className="text-2xl md:py-4 font-black ">{items.title}</h2>
             <p className="w-full">{items.text}</p>
+                {items.icons}
         </div>
     )
 })
@@ -36,7 +46,7 @@ const opcionesDisplay = opcioneArray.map((items) => {
 function Opciones() {
     return (
         <Zoom left>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-5 text-center h-80">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-5 text-center h-full p-6">
             {opcionesDisplay}
         </div>
         </Zoom>
--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -7,25 +7,28 @@ import { FaWhatsapp } from 'react-icons/fa';
 
 
 const Contactos = () => {
-    return (
-        <>
-            <div>
-                <h2 className="text-2xl md:text-3xl lg:text-4xl text-center py-7 font-extrabold">Contactos</h2>
-                <p className={"text-center"}>Para atender todas sus dudas estamos en todas las plataformas digitales.</p>
-                <div className="flex flex-col items-center md:flex-row justify-center p-8 md:gap-5">
-                <FaFacebookF className="text-5xl rounded facebook" />
-                <FaInstagramSquare className="text-5xl rounded instagram"/>
-                <FaYoutubeSquare className="text-5xl rounded youtube"/>
-                <FaTwitch className="text-5xl rounded twitch"/>
-                <FaWhatsapp className="text-5xl rounded twitch"/>
+  return (
+    <>
+      <div>
+        <h2 className='text-2xl md:text-3xl lg:text-4xl text-center py-7 font-extrabold'>Contactos</h2>
+        <p className={'text-center'}>Para atender todas sus dudas estamos en todas las plataformas digitales.</p>
+        <div className='flex flex-col items-center md:flex-row justify-center p-8 md:gap-5'>
+          <a href='https://www.facebook.com/profile.php?id=100071847725078' target='_blank'
+             rel='noreferrer'><FaFacebookF className='text-5xl rounded facebook' /></a>
+          <a href='https://instagram.com/difi_iot?utm_medium=copy_link' target='_blank'
+             rel='noreferrer'><FaInstagramSquare className='text-5xl rounded instagram' /></a>
+          <a href='https://www.youtube.com/channel/UCC5jKLTWSIHgRWN6tyKyw5g' target='_blank'
+             rel='noreferrer'><FaYoutubeSquare className='text-5xl rounded youtube' /></a>
+          <FaTwitch className='text-5xl rounded twitch' />
+          <FaWhatsapp className='text-5xl rounded twitch' />
 
-                </div>
-            </div>
+        </div>
+      </div>
 
-            <div className="flex flex-col justify-center items-center w-full ">
+      <div className='flex flex-col justify-center items-center w-full '>
 
-            </div>
-        </>
-    )
-}
-export default Contactos
+      </div>
+    </>
+  );
+};
+export default Contactos;
